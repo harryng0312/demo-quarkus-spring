05fc398e981ad40b1f371dffc43fdf8e9cc1b88b
harryng <harry@videabiz.com>
2022-03-29 15:53:54 +0700

    - add UI by qute

--- demo-quarkus-spring-web/src/main/java/com/example/ExampleResource.java
+++ demo-quarkus-spring-web/src/main/java/com/example/ExampleResource.java
@@ -4,7 +4,6 @@ import io.quarkus.logging.Log;
 import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,15 +17,15 @@ public class ExampleResource {
     @Location("pages/hello.html")
     private Template hello;
 
-    @RequestMapping(value = {"/",""}, method = RequestMethod.GET, produces = {MediaType.TEXT_PLAIN_VALUE})
+    @RequestMapping(value = {"/", ""}, method = RequestMethod.GET, produces = {MediaType.TEXT_PLAIN_VALUE})
     public String test(@RequestParam(value = "name", defaultValue = "") String name) {
-        Log.info("test: ${" + name + "}");
+        Log.info("test: " + name);
         return "Hello " + name;
     }
 
     @RequestMapping(value = {"/say-hello"}, method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
     public TemplateInstance sayHello(@RequestParam(value = "name", defaultValue = "") String name) {
-        Log.info("hello: ${" + name + "}");
+        Log.info("hello: " + name);
         return hello.data("name", name);
     }
 }
