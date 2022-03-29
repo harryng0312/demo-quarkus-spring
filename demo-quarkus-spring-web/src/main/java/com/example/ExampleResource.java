9a50d71c5ad14f3a58930c9ab5a6fdef15d58633
harryng <harry@videabiz.com>
2022-03-29 14:51:21 +0700

    - adjust multi modules

--- demo-quarkus-spring-web/src/main/java/com/example/ExampleResource.java
+++ demo-quarkus-spring-web/src/main/java/com/example/ExampleResource.java
@@ -1,15 +1,17 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import io.quarkus.logging.Log;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class ExampleResource {
 
-    @GetMapping
+    @RequestMapping(value = {"/",""}, method = RequestMethod.GET)
     public String hello() {
+        Log.info("test log");
         return "Hello Spring";
     }
 }
